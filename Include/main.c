b84630de00f48aaa00a039f53b93acdb8e4b208d
VTV02 <93756924+VTV02@users.noreply.github.com>
2024-05-29 14:19:18 +0700

    Create main.c

---
+++ Include/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<stdint.h>
+#include <uart_rx.h>
+#include<stm32f411xe.h>
+#define GPIOAEN			(1U<<0)
+
+char key;
+int main(void)
+{
+	RCC->AHB1ENR|=(1U<<0);
+	GPIOA->MODER|=(1U<<10);
+	GPIOA->MODER&=~(1U<<11);
+	uart2_txrx_init();
+	while(1)
+	{
+		key = uart2_read();
+		if(key=='1')
+		{
+			GPIOA->ODR|=(1U<<5);
+		}
+		if(key=='0')
+		{
+			GPIOA->ODR&=~(1U<<5);
+		}
+
+	}
+}
+
